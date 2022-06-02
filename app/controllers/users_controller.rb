ba330c2e9b68314541e56a8900664756baa9e9d0
backtofayton <yucelolcay@gmail.com>
2022-06-03 00:47:34 +0300

    Add user#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-  # def show
-  # end
+  def show
+    @user = User.find(params[:id]) rescue nil
+    @user_posts = Post.where(author_id: params[:id])
+    @user_posts_length = @user_posts.length
+    @user_posts = @user_posts.limit(3)
+  end
 end
