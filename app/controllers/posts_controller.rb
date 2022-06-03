7a48eee2bcce0ccf91eeee5fc3113d987a904fda
backtofayton <yucelolcay@gmail.com>
2022-06-03 23:22:51 +0300

    Add post#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,11 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @posts = Post.all.limit(2)
+    @posts.each {|post| if post.text.length > 200 then post.text = post.text.slice(0,200)+'..' end}
     @user_posts_count = user_posts_count(@user)
   end
   
   def show
-    @post = Post.find(params[:id]).most_recent_comments(5)
+    @post = Post.joins(:user).select('posts.*, users.name').find(params[:id])
   end
 end
