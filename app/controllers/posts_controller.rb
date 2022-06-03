1bc8340d1c177fcdfe44f070fb08df6434af47c7
backtofayton <yucelolcay@gmail.com>
2022-06-03 23:49:18 +0300

    Update

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,7 @@ class PostsController < ApplicationController
     page = [page, 1].max
     @user = User.find(params[:user_id])
     @posts = Post.all.slice((page * 2) - 2, page * 2)
+    # reduce post text
     @posts.each { |post| post.text = "#{post.text.slice(0, 200)}.." if post.text.length > 200 }
     @user_posts_count = user_posts_count(@user)
   end
