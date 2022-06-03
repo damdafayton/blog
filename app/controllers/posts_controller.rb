632ce76b0021c3342bce9ab08dfe87bc15abd680
backtofayton <yucelolcay@gmail.com>
2022-06-03 21:18:46 +0300

    Add posts#index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,11 @@
 class PostsController < ApplicationController
+  def index
+    @user = User.find(params[:user_id])
+    @posts = Post.all.limit(2)
+    @user_posts_count = user_posts_count(@user)
+  end
+  
+  def show
+    @post = Post.find(params[:id]).most_recent_comments(5)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
+  has_many :comments
+  has_many :likes
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
 
   def update_post_counter_of_user(bool_)
