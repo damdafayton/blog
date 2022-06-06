d65d7e98f1d8dd8f1e167ae04998dec6c0aa09ef
damdafayton <yucelolcay@gmail.com>
2022-06-06 16:20:59 +0300

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+
+first_post = Post.create(author_id: first_user, title: 'Hello', text: 'This is my first post')
+Post.create(author_id: first_user, title: 'Hello2', text: 'This is my first post_2')
+Post.create(author_id: first_user, title: 'Hello3', text: 'This is my first post_3')
+Post.create(author_id: first_user, title: 'Hello4', text: 'This is my first post_4')
+
+Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!' )
+Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!' )
+Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!' )
+Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!' )
+Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!' )
+Comment.create(post_id: first_post, author_id: second_user, text: 'Hi Tom!' )
