3270af338b11d7cdff5382bb36fb039d0fb1e747
backtofayton <yucelolcay@gmail.com>
2022-06-06 11:29:51 +0300

    Add foreign key associations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  has_many :posts
-  has_many :comments
+  has_many :posts, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id'
   has_many :likes, dependent: :destroy, class_name: 'Like', foreign_key: 'author_id'
 
   def most_recent_comments(limit_ = 3)
