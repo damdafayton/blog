c6b371243cd790062f86be354b46d454be41afa4
backtofayton <yucelolcay@gmail.com>
2022-06-07 14:03:47 +0300

    Refactor

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,12 +2,12 @@ class Like < ApplicationRecord
   belongs_to :post, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  after_save :update_like_counter_of_post
-  after_destroy -> { update_like_counter_of_post('down') }
+  after_save :update_likes_counter_of_post
+  after_destroy -> { update_likes_counter_of_post('down') }
 
   private
 
-  def update_comments_counter_of_post(arg = 'up')
+  def update_likes_counter_of_post(arg = 'up')
     case arg
     when 'up'
       post.increment!(:likes_counter)
