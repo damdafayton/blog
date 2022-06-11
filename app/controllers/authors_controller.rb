5e97ae5fb5118c201bec20b02585e04255eac85a
backtofayton <yucelolcay@gmail.com>
2022-06-11 16:06:15 +0300

    Add buttons for authentication

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,7 @@
-class AuthorsController < ApplicationController  
+class AuthorsController < ApplicationController
+  # p author_signed_in?
+  # p user_session\
+  
   def index
     @authors = User.all
   end
