1218a95bb72ffddc4ec95852aa2ffaa7f6e2680f
backtofayton <yucelolcay@gmail.com>
2022-06-12 00:39:51 +0300

    Add test variables

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+# require 'database_cleaner'
+
+# DatabaseCleaner.clean_with(:truncation)
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -6,8 +10,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', email: 'fake1@fake.com', password:'hel2131cli')
+second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', email: 'fake2@fake.com', password:'he1c314lli')
 
 first_post = Post.create(author_id: first_user, title: 'Hello', text: 'This is my first post')
 Post.create(author_id: first_user, title: 'Hello2', text: 'This is my first post_2')
--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,19 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  user = 1
-  describe "GET /authors/#{user}/posts" do
-    it "gets all posts of user #{user}" do
-      get author_posts_path(user)
+  before(:all) do
+    # load "#{Rails.root}/db/seeds.rb" 
+    @user = 1
+    @post = 2
+  end
+  
+  describe "GET /authors/1/posts" do  
+    it "gets all posts of user 1" do
+      get author_posts_path(@user)
       expect(response).to have_http_status(:ok)
       expect(response.body).to include('Likes:')
       expect(response).to render_template(:index)
     end
   end
 
-  describe "GET /authors/#{user}/posts/2" do
-    it "gets 2nd post of user #{user}" do
-      get author_post_path(user, 2)
+  describe "GET /authors/1/posts/2" do
+    it "gets 2nd post of user 1" do
+      get author_post_path(@user, @post)
       expect(response).to have_http_status(:ok)
       expect(response.body).to include('Comments:')
       expect(response).to render_template(:show)
