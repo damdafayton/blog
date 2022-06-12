3ea3041230625213e8359896a6c6740686583e80
backtofayton <yucelolcay@gmail.com>
2022-06-12 15:34:22 +0300

    Add

---
+++ spec/features/user_show_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+# I can see the user's profile picture.
+# I can see the user's username.
+# I can see the number of posts the user has written.
+# I can see the user's bio.
+# I can see the user's first 3 posts.
+# I can see a button that lets me view all of a user's posts.
+# When I click a user's post, it redirects me to that post's show page.
+# When I click to see all posts, it redirects me to the user's post's index page.
+
+RSpec.describe 'User show page,' do
+  describe 'renders' do
+    it 'user\'s profile picture' do
+    end
+
+    it 'user\'s name' do
+    end
+
+    it 'number of posts user has written' do
+    end
+
+    it 'user\'s bio' do
+    end
+
+    it 'user\'s first 2 posts' do
+    end
+
+    it 'button that let\'s me view user\'s all posts' do
+    end
+  end
+
+  it 'redirects me to that post\'s show page when I click on a user\'s post' do
+  end
+
+  it 'redirects me to the user\'s posts\' index page when I click to see all posts' do
+  end
+end
