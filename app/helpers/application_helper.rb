0a972867118056b7db35f4340ae69b53242acb91
backtofayton <yucelolcay@gmail.com>
2022-06-14 23:15:11 +0300

    Refactor design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def flash_messages_for(object)
     render(partial: 'layout/flash', locals: { object: })
   end
+
+  def image_control(image_url)
+    avatars = ["avatar1.png", "avatar2.png", "boy.png", "delivery_boy.png"]
+    unless image_url&.end_with?('png') or image_url&.end_with?('jpg') or image_url&.end_with?('gif') or image_url&.end_with?('jpeg')
+      return avatars[(rand*avatars.length).to_i]
+    end
+    return image_url
+  end
 end
