7d1dd4bf45d71ef8adca824e7ce9ae6089202d0e
backtofayton <yucelolcay@gmail.com>
2022-06-15 20:03:43 +0300

    Update smtp

--- config/environment.rb
+++ config/environment.rb
@@ -4,15 +4,17 @@ require_relative "application"
 # Initialize the Rails application.
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :address => 'smtp.sendgrid.net', 
-  :port => '587', 
-  :authentication => :login, 
-  :user_name => ENV['SENDGRID_USERNAME'], 
-  :password => ENV['SENDGRID_PASSWORD'], 
-  :domain => 'damdafayton-blog.herokuapp.com', 
-  :enable_starttls_auto => true 
-}
+# ActionMailer::Base.delivery_method = :smtp
+
+# ActionMailer::Base.smtp_settings = {
+#   :address => 'smtp.sendgrid.net', 
+#   :port => '587', 
+#   :authentication => :login, 
+#   :user_name => ENV['SENDGRID_USERNAME'], 
+#   :password => ENV['SENDGRID_PASSWORD'], 
+#   :domain => 'damdafayton-blog.herokuapp.com', 
+#   :enable_starttls_auto => true 
+# }
 
 # ActionMailer::Base.smtp_settings = {
 #   :port           => ENV['MAILGUN_SMTP_PORT'],
@@ -22,4 +24,3 @@ ActionMailer::Base.smtp_settings = {
 #   :domain         => 'yourapp.heroku.com',
 #   :authentication => :plain,
 # }
-# ActionMailer::Base.delivery_method = :smtp
