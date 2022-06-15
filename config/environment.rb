2559fbba7f84806053027e0d8bfebc150a43c96f
backtofayton <yucelolcay@gmail.com>
2022-06-15 19:42:26 +0300

    Update env

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,17 @@ ActionMailer::Base.smtp_settings = {
   :authentication => :plain, 
   :user_name => ENV['SENDGRID_USERNAME'], 
   :password => ENV['SENDGRID_PASSWORD'], 
-  :domain => 'heroku.com', 
+  :domain => 'damdafayton-blog.herokuapp.com', 
   :enable_starttls_auto => true 
 }
+ActionMailer::Base.delivery_method = :smtp
+
+# ActionMailer::Base.smtp_settings = {
+#   :port           => ENV['MAILGUN_SMTP_PORT'],
+#   :address        => ENV['MAILGUN_SMTP_SERVER'],
+#   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+#   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+#   :domain         => 'yourapp.heroku.com',
+#   :authentication => :plain,
+# }
+# ActionMailer::Base.delivery_method = :smtp
