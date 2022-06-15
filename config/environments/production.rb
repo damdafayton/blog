d2c31b7e3701ea2fce232b22091bf92ce194f283
backtofayton <yucelolcay@gmail.com>
2022-06-15 19:25:51 +0300

    Add smtp server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,5 +90,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'damdafayton-blog.herokuapp.com' }
+
+  config.action_mailer.default_url_options = { :host => 'damdafayton-blog.herokuapp.com', :protocol => 'https' }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
 end
